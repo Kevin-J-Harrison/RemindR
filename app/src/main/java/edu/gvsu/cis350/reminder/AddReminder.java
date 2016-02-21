9071f6d2321c3023ff8d983e6fa0070695094a30
arwin915 <arwin915@gmail.com>
2016-02-20 23:25:41 -0500

    Classes almost implmented. List and Add Activities should be nearly functional. Currently crashes to a null pointer exception. Have yet to fix.
    Andrew

--- app/src/main/java/edu/gvsu/cis350/reminder/AddReminder.java
+++ app/src/main/java/edu/gvsu/cis350/reminder/AddReminder.java
@@ -4,6 +4,7 @@ import android.app.DatePickerDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
 import android.content.Intent;
+import android.os.Parcelable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -13,26 +14,41 @@ import android.widget.DatePicker;
 import android.widget.EditText;
 
 import java.util.Calendar;
+import java.util.Date;
+
 /**
 * Written by Andrew Burns
 **/
 
 
 public class AddReminder extends AppCompatActivity {
-
+    public static DoubleLinkedList reminderList;
+    EditText titleText = (EditText) findViewById(R.id.ReminderName);
+    EditText notesText = (EditText) findViewById(R.id.notesField);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_reminder);
         Intent intent = getIntent();
-        //String value = intent.getStringExtra("key"); //if it's a string you stored.
+        reminderList = intent.getParcelableExtra("reminders");
 
         Button submit = (Button) findViewById(R.id.submit_button);
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                //create new reminder and add it to the list
+                Reminder temp = new Reminder();
+                temp.setTitle(titleText.getText().toString());
+                temp.setNotes(notesText.getText().toString());
+                temp.setDate(new Date());
+                //unsure how to grab info from time spinners and then properly format into Date
+                //for now just setting to current date and time
+
+                reminderList.add(temp);
+
                 Intent myIntent = new Intent(AddReminder.this, ViewReminders.class);
-                //myIntent.putExtra("key", value); //Optional parameters
+                myIntent.putExtra("reminders", (Parcelable)reminderList); //Optional parameters
                 AddReminder.this.startActivity(myIntent);
             }
         });
--- app/src/main/java/edu/gvsu/cis350/reminder/ViewReminders.java
+++ app/src/main/java/edu/gvsu/cis350/reminder/ViewReminders.java
@@ -2,6 +2,7 @@ package edu.gvsu.cis350.reminder;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -9,13 +10,16 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import java.util.Date;
 
 
 public class ViewReminders extends AppCompatActivity {
 
-    public static DoubleLinkedList reminderList = new DoubleLinkedList();
-    FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+    public DoubleLinkedList reminderList;
+
 
 
 
@@ -23,27 +27,63 @@ public class ViewReminders extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_reminders);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        Button fab = (Button) findViewById(R.id.button);
+        if (reminderList == null)
+            reminderList  = new DoubleLinkedList();
 
+        //grab any passed variables
         Intent intent = getIntent();
-        //String value = intent.getStringExtra("key"); //if it's a string you stored.
+        reminderList = intent.getParcelableExtra("reminders");
+
 
 
         //Add Reminder Button
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                //get ready to go to the next activity, pass the list through
                 Intent myIntent = new Intent(ViewReminders.this, AddReminder.class);
-                //myIntent.putExtra("key", value); //Optional parameters
+                myIntent.putExtra("reminders", (Parcelable)reminderList);
                 ViewReminders.this.startActivity(myIntent);
             }
         });
 
         //generate the list of reminders
-        if (reminderList.size() != 0){
-
+        if (reminderList != null){
+            populateListView();
         }
+        else{
+            /*quick test list generator*/
+            Reminder test = new Reminder();
+            for (int i = 1; i < 4; i++){
+                test.setTitle(("Reminder #" +  Integer.toString(i)));
+                test.setNotes(("These are notes. " + Integer.toString(i)));
+                test.setDate(new Date());
+                reminderList.add(test);
+            }
+            populateListView();
+        }
+    }
+
+    private void populateListView(){
+        //Create list of items
+        int size = reminderList.size();
+        String[] reminderFinalListView = new String[size];
+
+        Reminder temp;
+        //build an array of titles
+        for (int i = 0; i < size; i++) {
+            temp = (Reminder)reminderList.get(i);
+            reminderFinalListView[i] = (temp.getTitle() + "\n" + temp.getDate().toString());
+        }
+
+        //Build adapter
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.list_cell, reminderFinalListView);
+
+        //Configure ListView
+        ListView list = (ListView) findViewById(R.id.reminderListView);
+        list.setAdapter(adapter);
+
     }
 
     @Override
