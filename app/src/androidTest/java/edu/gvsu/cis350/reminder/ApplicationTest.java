46136d240e0ba85201158383097f4904a624ee22
Kevin Harrison <kharrison2797@gmail.com>
2016-03-23 15:47:41 -0400

    Working Gradle for Espresso testing, I believe

--- app/src/androidTest/java/edu/gvsu/cis350/reminder/ApplicationTest.java
+++ app/src/androidTest/java/edu/gvsu/cis350/reminder/ApplicationTest.java
@@ -2,6 +2,9 @@ package edu.gvsu.cis350.reminder;
 
 import android.app.Application;
 import android.test.ApplicationTestCase;
+import android.support.test.runner.AndroidJUnit4;
+import android.support.test.runner.AndroidJUnitRunner;
+import android.test.ActivityInstrumentationTestCase2;
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
