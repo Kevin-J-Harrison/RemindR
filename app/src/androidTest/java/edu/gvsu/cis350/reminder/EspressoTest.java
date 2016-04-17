026a7329642fc154ba9cddbb8b1b93b5b4292595
Alexander Vansteel <alexandervansteel@gmail.com>
2016-04-16 23:23:46 -0400

    no message

--- app/src/androidTest/java/edu/gvsu/cis350/reminder/EspressoTest.java
+++ app/src/androidTest/java/edu/gvsu/cis350/reminder/EspressoTest.java
@@ -12,7 +12,9 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.clearText;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.scrollTo;
 import static android.support.test.espresso.action.ViewActions.typeText;
@@ -20,6 +22,7 @@ import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.withClassName;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.anything;
 
 /**
  * Created by alexvansteel on 3/23/16.
@@ -61,7 +64,7 @@ public class EspressoTest {
 
         onView(withClassName(Matchers.equalTo(DatePicker.class.getName()))).perform(scrollTo());
         onView(withClassName(Matchers.equalTo(DatePicker.class.getName())))
-                .perform(PickerActions.setDate(year, month + 1, day));
+                .perform(PickerActions.setDate(year, month, day));
 
         /**
          * Sets a time in the TimePicker
@@ -79,7 +82,7 @@ public class EspressoTest {
          */
         onView(withId(R.id.editAddressField)).perform(scrollTo());
         onView(withId(R.id.editAddressField)).perform(typeText("1 Campus Drive"));
-        
+
         /**
          * Sets the notes
          */
@@ -93,79 +96,44 @@ public class EspressoTest {
         onView(withId(R.id.saveEditbutton)).perform(click());
 
 
-    /**
-     * Clicks on the Reminder and verifies the title is the title of the reminder.
-     */
+        /**
+         * Clicks on the Reminder and verifies the title is the title of the reminder.
+         */
+        onData(anything())
+                .inAdapterView(withId(android.R.id.list))
+                .atPosition(0)
+                .perform(click());
+        /**
+         * Clicks on the edit button
+         */
+        onView(withId(R.id.editButton)).perform(click());
+
+        /**
+         * Edits the fields.
+         */
+        onView(withId(R.id.displayReminderName)).perform(scrollTo(), clearText(), typeText("New Title"));
+        onView(withId(R.id.displayReminderName)).check(matches(withText("New Title")));
+
+        onView(withId(R.id.editAddressField)).perform(scrollTo(), clearText(), typeText("DeVos"));
+        onView(withId(R.id.editAddressField)).check(matches(withText("DeVos")));
+
+        onView(withId(R.id.editNotesField)).perform(scrollTo(), clearText(), typeText("spam spam eggs spam"));
+        onView(withId(R.id.editNotesField)).check(matches(withText("spam spam eggs spam")));
+
+        onView(withId(R.id.saveEditbutton)).perform(scrollTo(), click());
+
+        /**
+         * Opens and deletes reminder.
+         */
+        onData(anything())
+                .inAdapterView(withId(android.R.id.list))
+                .atPosition(0)
+                .perform(click());
+
+        onView(withId(R.id.editButton)).perform(click());
+
+        onView(withId(R.id.deleteButton)).perform(scrollTo(), click());
 
     }
-
-    /**
-     * Clicks on the Reminder in the list view, then chooses the option to edit the reminder.
-     * Values are changed and the reminder is saved. Then it is verified that the changes were updated.
-     */
-
-//
-//    /**
-//     * Clicks on the Reminder in the list view, then chooses the option to edit the reminder.
-//     * Values are changed and the reminder is saved. Then it is verified that the changes were updated.
-//     */
-//    //@Test
-//    public void editReminderTest() {
-//
-//        onView(withId(R.id.editButton)).perform(click());
-//
-//        onView(withId(R.id.ReminderName)).perform(typeText("New Reminder Title"));
-//        onView(withId(R.id.notesField)).perform(typeText("Edit reminder notes"));
-//
-//        /**
-//         * Specifically for datePicker
-//         */
-//        int year = 2018;
-//        int month = 1;
-//        int day = 25;
-//
-//        onView(withId(R.id.datePicker)).perform(click());
-//        onView(withClassName(Matchers.equalTo(DatePicker.class.getName())))
-//                .perform(PickerActions.setDate(year, month + 1, day));
-//        onView(withId(android.R.id.button1)).perform(click());
-//
-//        /**
-//         * Specificaly for timePicker
-//         */
-//        int hour = 5;
-//        int minutes = 43;
-//
-//        onView(withId(R.id.timePicker)).perform(click());
-//        onView(withClassName(Matchers.equalTo(TimePicker.class.getName())))
-//                .perform(PickerActions.setTime(hour, minutes));
-//        onView(withId(android.R.id.button1)).perform(click());
-//
-//
-//        onView(withId(R.id.saveEditbutton)).perform(click());
-//
-//        onView(withId(R.id.ReminderName)).check(matches(withText("New Reminder Title")));
-//        onView(withId(R.id.notesField)).check(matches(withText("Edit reminder notes")));
-//        onView(withId(R.id.dateText)).check(matches(withText(year + "/" + month + "/" + day)));
-//        onView(withId(R.id.timeText)).check(matches(withText(hour + ":" + minutes)));
-//    }
-//
-//    /**
-//     * Clicks on the Reminder, then deletes the reminder. Then checks to make sure the list is null.
-//     */
-//    //@Test
-//    public void deleteReminderTest() {
-//
-//        onData(withId(reminderListView)).perform(click());
-//
-//        onView(withId(R.id.editButton)).perform(scrollTo());
-//        onView(withId(R.id.editButton)).perform(click());
-//
-//        onView(withId(R.id.deleteButton)).perform((scrollTo()));
-//        onView(withId(R.id.deleteButton)).perform(click());
-//
-//        onData(withId(reminderListView)).check(null);
-//    }
-//
-//
 }
 
